b4c60ce3786b3e7aa13f1d5cd6845bb9e31f62dd
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-04-22 14:42:12 +0530

    Learning condition in py

---
+++ chapter7exercise/exercise1.py
@@ -0,0 +1,25 @@
+a1 = int(input("Enter the num 1: "))
+a2 = int(input("Enter the num 2: "))
+a3 = int(input("Enter the num 3: "))
+a4 = int(input("Enter the num 4: "))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("Greatest Num is a1: ",a1)
+
+if(a2>a1 and a2>a3 and a2>a4):
+    print("Greatest Num is a2: ",a2)
+
+if(a3>a2 and a3>a1 and a3>a4):
+    print("Greatest Num is a3: ",a3)
+
+if(a4>a2 and a4>a3 and a4>a1):
+    print("Greatest Num is a4: ",a4)
+
+# else:
+#     print("all are equal")
+
+
+
+
+
+
---
+++ chapter7exercise/prob.py
@@ -0,0 +1,3 @@
+marks1 = int(input("enter the marks 1"))
+marks2 = int(input("enter the marks 2"))
+marks3 = int(input("enter the marks 3"))
