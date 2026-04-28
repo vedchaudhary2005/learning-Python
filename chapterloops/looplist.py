a89bd35ae72631e549eac6d8ff903aea6544b9cc
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-04-28 22:36:14 +0530

    Learning break and continue in python

--- chapterloops/looplist.py
+++ chapterloops/looplist.py
@@ -13,7 +13,13 @@
 #     print("DONE ")
 
 
+# for i in range(100):
+#     if(i==35):
+#         break
+#     print(i)
+
+
 for i in range(100):
     if(i==35):
-        break
+        continue
     print(i)
