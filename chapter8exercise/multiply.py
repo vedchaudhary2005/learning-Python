39f929ecd80a22ceabd7da1f626763ec2a1b7c93
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-04-28 23:02:30 +0530

    Doing practical of Loops

---
+++ chapter8exercise/multiply.py
@@ -0,0 +1,4 @@
+n = int(input("enter a num "))
+for i in range(1, 11):
+    print(f"{n} X {i} = {n * i}")
+    i=+1
--- chapterloops/pass.py
+++ chapterloops/pass.py
@@ -1,7 +1,11 @@
-for i in range(550):
-    pass
+# for i in range(550):
+#     pass
 
-i = 0
-while(i<45):
+# i = 0
+# while(i<45):
+#     print(i)
+#     i +=1
+i=1
+for i in range(11):
     print(i)
-    i +=1
+    i+=1
