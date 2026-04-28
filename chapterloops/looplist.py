0cac30cde76844fb3f1d8257c532ea77800d0872
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-04-28 22:34:29 +0530

    Learning break and continue in python

--- chapterloops/looplist.py
+++ chapterloops/looplist.py
@@ -1,13 +1,19 @@
-l = [1, 5, "ved", False, 454, 45.5 ]
-i = 0
-while(i < len(l)):
-    print(l[i])
-    i += 1
+# l = [1, 5, "ved", False, 454, 45.5 ]
+# i = 0
+# while(i < len(l)):
+#     print(l[i])
+#     i += 1
 
 
-t = (1,5,88,98,"2")
-i = 0
-for i in t:
+# t = (1,5,88,98,"2")
+# i = 0
+# for i in t:
+#     print(i)
+# else:
+#     print("DONE ")
+
+
+for i in range(100):
+    if(i==35):
+        break
     print(i)
-else:
-    print("DONE")
