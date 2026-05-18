4c12be6edb195bfbe05e004c5e73bf428bc4b3a8
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-05-18 15:00:55 +0530

    Doing practical of Loops

--- chapter8exercise/star.py
+++ chapter8exercise/star.py
@@ -3,9 +3,8 @@
 # * * * * *
 
 
-
 n = int(input("Enter the number: "))
-for i in range(1, n+1):
+for i in range(1, n+1): 
     print(" "* (n-i), end="")
     print("*"* (2*i-1), end="")
     print("")
