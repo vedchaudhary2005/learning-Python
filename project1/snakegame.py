fcf7c4ee1a778f1cd4d856da56bb6e79c4b15fe2
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-06-18 22:58:45 +0530

    Build game using python

--- project1/snakegame.py
+++ project1/snakegame.py
@@ -1,1 +1,8 @@
-           
+computer = 1
+youstr =  input("enter the choice: ")
+youDict = {"s":1, "w":2 , "g":3}
+you = youDict(youstr)
+
+if(computer == 1 and you == 2):
+    print("You win")
+elif(         )
