e93c1017d5e1a69c873db035c22ec81d103f3da9
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-06-26 14:00:39 +0530

    Initial commit

--- project1/snakegame.py
+++ project1/snakegame.py
@@ -4,5 +4,13 @@ youDict = {"s":1, "w":2 , "g":3}
 you = youDict(youstr)
 
 if(computer == 1 and you == 2):
-    print("You win")
- 
+    print("You loss")
+
+elif(computer == 1  and you==3):
+    print("You win")   
+elif(computer == 1 and you==1):
+    print("Game Draw")  
+elif(computer == 2 and you==2):
+    print("Game Draw") 
+elif(computer == 3 and you==3):
+    print("Game Draw") 
