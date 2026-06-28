70b577acb436538c6b295b891c28de7f38dda85b
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-06-28 13:59:43 +0530

    Initial commit

--- project1/snakegame.py
+++ project1/snakegame.py
@@ -1,16 +1,41 @@
-computer = 1,2,3
-youstr =  input("enter the choice: ")
-youDict = {"s":1, "w":2 , "g":3}
-you = youDict(youstr)
+import random
 
-if(computer == 1 and you == 2):
-    print("You loss")
 
-elif(computer == 1  and you==3):
-    print("You win")   
-elif(computer == 1 and you==1):
-    print("Game Draw")  
-elif(computer == 2 and you==2):
-    print("Game Draw") 
-elif(computer == 3 and you==3):
-    print("Game Draw")    
+computer = random.randint(1, 3)
+youstr = input("Enter your choice (s for Stone, w for Water, g for Gun): ")
+
+
+youDict = {"s": 1, "w": 2, "g": 3}
+
+you = youDict[youstr]
+
+
+print("Computer chose:", computer)
+print("You chose:", you)
+
+if computer == 1 and you == 2:
+    print("You Lose")
+
+elif computer == 1 and you == 3:
+    print("You Win")
+
+elif computer == 1 and you == 1:
+    print("Game Draw")
+
+elif computer == 2 and you == 1:
+    print("You Win")
+
+elif computer == 2 and you == 3:
+    print("You Lose")
+
+elif computer == 2 and you == 2:
+    print("Game Draw")
+
+elif computer == 3 and you == 1:
+    print("You Lose")
+
+elif computer == 3 and you == 2:
+    print("You Win")
+
+elif computer == 3 and you == 3:
+    print("Game Draw")
