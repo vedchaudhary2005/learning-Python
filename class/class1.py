fb6741c28925757cc84ae4bc6aa8dbf6e3afa93d
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-07-02 22:10:32 +0530

    Initial commit

---
+++ class/class1.py
@@ -0,0 +1,7 @@
+class employee:
+    name= "ved"
+    lang ="py"
+    sal= 124555
+
+harry = employee()
+print(harry.sal)
---
+++ fileio/append.py
@@ -0,0 +1,4 @@
+st = " you r good"
+f = open("myfile.txt", "a")
+f.write(st)
+f.close
